be5dc44753d725cf145f6dc1e32370cacfc8cef2
Ethan Teo <ethanteo@asu.edu>
2026-09-26 06:55:45 +0000

    Reordered the rtl file list

--- LAB1/rtl/rtl.f
+++ LAB1/rtl/rtl.f
@@ -1,8 +1,8 @@
 // Include headers from the FIFO directory
 +incdir+$PROJECT_ROOT/rtl/fifo
 
-// Include the FIFO file list
--f $PROJECT_ROOT/rtl/fifo/fifo.f
-
 // Include the Even Odd file list
 -f $PROJECT_ROOT/rtl/even_odd/even_odd.f
+
+// Include the FIFO file list
+-f $PROJECT_ROOT/rtl/fifo/fifo.f
